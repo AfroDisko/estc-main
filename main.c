1c1577ccabaf859921cc176ebdb88ddae75cd94d
AfroDisko <calexei76@gmail.com>
2022-10-25 19:58:40 +0500

    minor opts

--- main.c
+++ main.c
@@ -6,33 +6,31 @@
 #define DELAY_BLINK 250
 #define DELAY_PAUSE 1000
 
-void Nblink(int id, int N)
+void Nblink(int idx, int N)
 {
     for(int cntr = 0; cntr < 2 * N - 1; ++cntr)
     {
-        bsp_board_led_invert(id);
+        bsp_board_led_invert(idx);
         nrf_delay_ms(DELAY_BLINK);
     }
 
-    bsp_board_led_invert(id);
+    // just in case
+    if(N > 0) 
+        bsp_board_led_invert(idx);
 }
 
 int main(void)
 {
     bsp_board_init(BSP_INIT_LEDS);
 
-    while (true)
+    int nBlinks[4] = {7, 1, 9, 7};
+
+    while(true)
     {
-        Nblink(0, 7);
-        nrf_delay_ms(DELAY_PAUSE);
-
-        Nblink(1, 1);
-        nrf_delay_ms(DELAY_PAUSE);
-
-        Nblink(2, 9);
-        nrf_delay_ms(DELAY_PAUSE);
-
-        Nblink(3, 7);
-        nrf_delay_ms(DELAY_PAUSE);
+        for(int idx = 0; idx < LEDS_NUMBER; ++idx)
+        {
+            Nblink(idx, nBlinks[idx]);
+            nrf_delay_ms(DELAY_PAUSE);
+        }
     }
 }
